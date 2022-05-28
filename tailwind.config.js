dd6c54f05576f3bc245a12ec0bdc258e7a0594dc
Zubair <zubairimtiaz395@gmail.com>
2022-05-28 12:20:51 +0500

    V1 of figma to responsive

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Rampart: ["Rampart One", "cursive"],
+        Roboto: ["Roboto", "sans-serif"],
+       }
+    },
+  },
+  plugins: [],
+}
