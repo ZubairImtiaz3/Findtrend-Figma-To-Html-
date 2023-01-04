b86f9a42e3ce743c09f1f90032fcd279f59294ca
Zubair Imtiaz <zubairimtiaz395@gmail.com>
2023-01-04 06:49:40 +0500

    Nav Improved

--- assets/scriptNavbar.js
+++ assets/scriptNavbar.js
@@ -1,6 +1,12 @@
 // Get the navbar element
 let navbar = document.getElementById("stickyNavbar");
 
+//Get the HAMBURGER
+let crossHam = document.getElementById("navCross");
+
+//Get the NavLinks
+const navLinks = document.querySelectorAll(".navLinks");
+
 // When the user scrolls down, add the "scrolled" class to the navbar
 window.onscroll = function () {
   if (window.scrollY <= 0) {
@@ -9,3 +15,16 @@ window.onscroll = function () {
     navbar.classList.add("stickyNavWidthHeight");
   }
 };
+
+//Hamburger Converts To Cross
+crossHam.addEventListener("click", function () {
+  this.classList.toggle("active");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    // Close the menu when a nav link is clicked
+    const menu = document.querySelector("#hamButton");
+    menu.setAttribute("aria-expanded", "false");
+  });
+});
